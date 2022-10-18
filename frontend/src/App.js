ccd8ebd5f7246938cfd97dc78a3a925450a1fcfa
LONGMANKE <mburunjoroge0@gmail.com>
2022-10-18 13:44:23 +0100

    overlay navbar in frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router } from "react-router-dom"
+import Header from "./component/layout/Header/Header.js"
+
+
+function App() {
+  return (
+    <Router>
+    <Header />
+    </Router>
+  );  
+}
+
+export default App;
---
+++ frontend/src/component/layout/Header/Header.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { ReactNavbar } from "overlay-navbar";
+import logo from "../../../images/logo.png";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
+const options = {
+  burgerColorHover: "#eb4034",
+  logo,
+  logoWidth: "20vmax",
+  navColor1: "white",
+  logoHoverSize: "10px", 
+  logoHoverColor: "#eb4034",
+  link1Text: "Home",
+  link2Text: "Services", 
+  link3Text: "Contact",
+  link4Text: "About",
+  link1Url: "/",
+  link2Url: "/services",
+  link3Url: "/contact",
+  link4Url: "/about",
+  link1Size: "1.3vmax",
+  link1Color: "rgba(35, 35, 35,0.8)",
+  nav1justifyContent: "flex-end",
+  nav2justifyContent: "flex-end",
+  nav3justifyContent: "flex-start",
+  nav4justifyContent: "flex-start",
+  link1ColorHover: "#eb4034",
+  link1Margin: "1vmax",
+  profileIconUrl: "/login",
+  searchIconUrl: "/search",
+  cartIconUrl: "/cart",
+  profileIconColor: "rgba(35, 35, 35,0.8)",
+  searchIconColor: "rgba(35, 35, 35,0.8)",
+  cartIconColor: "rgba(35, 35, 35,0.8)",
+  profileIconColorHover: "#eb4034",
+  searchIconColorHover: "#eb4034",
+  cartIconColorHover: "#eb4034",
+  cartIconMargin: "1vmax",
+};
+
+const Header = () => {
+  return <ReactNavbar
+    {...options}
+    profileIcon="true"
+    ProfileIconElement={AccountCircleIcon }
+    searchIcon="true"
+    SearchIconElement={SearchIcon}
+    cartIcon="true"
+    CartIconElement={ShoppingCartIcon}
+    searchIconSize="2.5vmax"
+    cartIconWidth="2.5vmax"
+    fontSize="large"
+  />;
+};
+
+export default Header;
