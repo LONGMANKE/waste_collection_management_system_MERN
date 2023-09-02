5741fcae8adda59492033d865f565239cf4e0643
LONGMANKE <mburunjoroge0@gmail.com>
2023-09-02 17:21:54 +0300

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,13 @@ process.on("uncaughtException", (err) => {
 //config
 
 // dotenv.config({ path: "backend/config/config.env" });
-if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({ path: "backend/config/config.env" });
+
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
   }
  
 //connecting with database 
